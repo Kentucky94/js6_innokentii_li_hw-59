d88c16836b5b5d08757f67f34fedd2b31c74803b
Kentucky94 <lis5043@gmail.com>
2019-12-06 00:42:48 +0600

    Added first task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import nanoid from 'nanoid';
+
+import AddMovieForm from "./Components/AddMovieForm/AddMovieForm";
+import MovieTab from "./Components/Movie/MovieTab";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: '',
+    movies: [],
+  };
+
+  changeText = (event) => {
+    const text = event.target.value;
+
+    this.setState({text: text})
+  };
+
+  addMovie = () => {
+    const movies = [...this.state.movies];
+    const newTask = {text: this.state.text, id: nanoid()};
+
+    movies.push(newTask);
+
+    this.setState({movies});
+  };
+
+  deleteMovie = (id) => {
+    const movies = [...this.state.movies];
+    const movieIndex = movies.findIndex(task => task.id === id);
+
+    movies.splice(movieIndex, 1);
+
+    this.setState({movies});
+  };
+
+  editText = (event, id) => {
+    const movies = [...this.state.movies];
+    const index = movies.findIndex(movie => movie.id === id);
+    const movie = {...movies[index]};
+
+    movie.text = event.target.value;
+
+    movies[index] = movie;
+
+    this.setState({movies})
+  };
+
+  render() {
+    const movies = this.state.movies.map(movie => (<MovieTab
+      key={movie.id}
+      text={movie.text}
+      delete={() => this.deleteMovie(movie.id)}
+      editText={(event) => this.editText(event, movie.id)}
+    />));
+
+    return (
+      <div className='App'>
+        <div className="MovieApp">
+          < AddMovieForm onChange={this.changeText} add={this.addMovie}/>
+          <div>
+            {movies}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Movie/MovieTab.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+import './MovieTab.css';
+
+class MovieTab extends Component {
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return this.props.text !== nextProps.text;
+  }
+
+  componentDidUpdate() {
+    console.log('[Post] DidUpdate');
+  }
+
+  render() {
+    return (
+      <div className='task_tab'>
+        <input type="text" value={this.props.text} onChange={this.props.editText}/>
+        <button className='task_btn' onClick={this.props.delete}>Delete</button>
+      </div>
+    );
+  }
+}
+
+export default MovieTab;
